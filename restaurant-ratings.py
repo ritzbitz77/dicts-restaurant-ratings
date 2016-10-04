b8226acba919c7811c2b0fe1c1a904956cab121a
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-03 18:06:15 -0700

    Updated with user prompts

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -10,18 +10,36 @@ def sort_ratings(restaurant_file):
 
             alpha_ratings[restaurant_name] = restaurant_rating
 
+        while True:
+            print "Would you like to see the list, add to the list, or quit?"
+            user_answer = raw_input("Enter 1 to see the list, enter 2 to add to the list, enter 3 to quit.")
+            if user_answer == "1":
+                alpha_ratings = sorted(alpha_ratings.items())        
 
-        new_restaurant = raw_input("What is the name of the restaurant that are rating?: ")
+                for restaurant_info in alpha_ratings:
+                    sorted_restaurant_name = restaurant_info[0]
+                    sorted_restaurant_rating = int(restaurant_info[1])
 
-        alpha_ratings[new_restaurant] = int(raw_input ("What rating would you give this restaurant?: "))
+                    print "%s is rated at %d." % (sorted_restaurant_name, sorted_restaurant_rating)
 
-        alpha_ratings = sorted(alpha_ratings.items())        
+            elif user_answer == "2":
+                new_restaurant = raw_input("What is the name of the restaurant that are rating?: ")
 
-        for restaurant_info in alpha_ratings:
-            sorted_restaurant_name = restaurant_info[0]
-            sorted_restaurant_rating = int(restaurant_info[1])
+                alpha_ratings[new_restaurant] = int(raw_input ("What rating would you give this restaurant?: "))
 
-            print "%s is rated at %d." % (sorted_restaurant_name, sorted_restaurant_rating)
+
+
+                alpha_ratings = sorted(alpha_ratings.items())        
+
+                for restaurant_info in alpha_ratings:
+                    sorted_restaurant_name = restaurant_info[0]
+                    sorted_restaurant_rating = int(restaurant_info[1])
+
+                    print "%s is rated at %d." % (sorted_restaurant_name, sorted_restaurant_rating)
+
+
+            elif user_answer == "3":
+                break
 
 
 sort_ratings("scores.txt")
